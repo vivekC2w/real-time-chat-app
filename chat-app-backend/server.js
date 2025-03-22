08d1a9477534cf9e1cd007f8300799e389e15b31
Vivek <vivek.hiray@alt-mobility.com>
2025-03-22 12:08:49 +0530

    Integrated redis, implemented real-time messaging with websockets & offline message handling

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const cors = require("cors");
+const { createClient } = require("redis");
 
 const app = express();
 const server = http.createServer(app);
@@ -16,9 +17,41 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+const redisClient = createClient({ url: process.env.REDIS_URL });
+redisClient.connect().catch(console.error);
 
 io.on("connection", (socket) => {
     console.log(`A user connected: ${socket.id}`);
+
+    socket.on("sendMessage", async (msgData) => {
+        const {senderId, receiverId, content, type} = msgData;
+
+        //storing msg in database
+        const msg = await Prisma.message.create({
+            data: { senderId, receiverId, content, type }
+        });
+
+        //store undelivered msg in Redis if the recipient is offline
+        const recipientSocketId = activeUsers[receiverId];
+        if (recipientSocketId) {
+            io.to(recipientSocketId).emit("receiveMessage", msg);
+        } else {
+            await redisClient.lPush(`offline:${receiverId}`, JSON.stringify(msg));
+        }
+    })
+
+    socket.on("userOnline", async (userId) => {
+        activeUsers[userId] = socket.id;
+
+        //send offline msgs if any exists
+        const msgs = await redisClient.lRange(`offline:${userId}`, 0, -1);
+        if (msgs.length > 0) {
+            msgs.forEach((msg) => {
+                io.to(socket.id).emit("receiveMessage", JSON.parse(msg));
+            });
+            await redisClient.del(`offline:${userId}`);
+        }
+    });
 
     socket.on("disconnected", () => {
         console.log(`A user disconnected: ${socket.id}`);
